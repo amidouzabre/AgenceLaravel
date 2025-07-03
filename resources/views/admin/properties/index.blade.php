c392be36e8f362cc9046f58a35e3abf3e8687d28
amidou.zabre <amidou.zabre@gmail.com>
2025-07-03 11:42:48 +0000

    Views->admin->properties->index: change title

--- resources/views/admin/properties/index.blade.php
+++ resources/views/admin/properties/index.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.admin')
 
-@section('title', 'Properties')
+@section('title', 'Tous les biens')
 
 @section('content')
     <div class="d-flex justify-content-between align-items-center">
-        <h1>Properties</h1>
+        <h1>@yield('title')</h1>
         <a href="{{ route('admin.property.create') }}" class="btn btn-primary">Ajouter un bien</a>
     </div>
     <table class="table table-striped">
