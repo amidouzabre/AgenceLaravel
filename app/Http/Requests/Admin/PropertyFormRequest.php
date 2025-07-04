21f04d97c598fb8e240d310ef6f353f808d446e7
amidou.zabre <amidou.zabre@gmail.com>
2025-07-04 15:46:02 +0000

    PropertyFormRequest: add options

--- app/Http/Requests/Admin/PropertyFormRequest.php
+++ app/Http/Requests/Admin/PropertyFormRequest.php
@@ -33,6 +33,7 @@ class PropertyFormRequest extends FormRequest
             'address' => ['required', 'string', 'min:8'],
             'postal_code' => ['required', 'string', 'min:3'],
             'sold' => ['required', 'boolean'],
+            'options' => ['required', 'array', 'exists:options,id'],
         ];
     }
 }
