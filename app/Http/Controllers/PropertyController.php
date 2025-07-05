5b4d872dac7f5b12d1075c346ad6f51315ec6412
amidou.zabre <amidou.zabre@gmail.com>
2025-07-05 15:36:00 +0000

    Requests: create SearchPropertiesRequest

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchPropertiesRequest;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    public function index()
+    public function index(SearchPropertiesRequest $request)
     {
-        $properties = Property::paginate(16);
-        return view('property.index', ['properties' => $properties]);
+        $query = Property::query();
+        
+        if($request->has('price')){
+            $query->where('price', '<=', $request->input('price'));
+        }
+
+        return view('property.index', ['properties' => $query->paginate(16)]);
     }
 
     public function show(string $slug, Property $property)
