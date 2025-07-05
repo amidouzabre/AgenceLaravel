2522a1c839751e336bec9b577ef5af20640ca636
amidou.zabre <amidou.zabre@gmail.com>
2025-07-05 10:00:02 +0000

    Vues: create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('base')
+
+@section('content')
+    <div class="bg-light p-5 text-center">
+        <div class="container">
+            <h1 class="display-4">Bienvenue sur la page d'accueil</h1>
+            <p class="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+    </div>
+    <div class="container">
+        <h2>Nos derniers biens</h2>
+        <div class="row">
+            @foreach($properties as $property)
+                <div class="col">
+                        sss
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
