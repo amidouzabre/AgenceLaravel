6b68394e6ed4d1e4d3ce41742dc719affe4d7b77
amidou.zabre <amidou.zabre@gmail.com>
2025-07-05 15:52:00 +0000

    Controllers->PropertyController: check conditions

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -12,8 +12,20 @@ class PropertyController extends Controller
     {
         $query = Property::query();
         
-        if($request->has('price')){
-            $query->where('price', '<=', $request->input('price'));
+        if($price = $request->validated('price')){
+            $query->where('price', '<=', $price);
+        }
+
+        if($surface = $request->validated('surface')){
+            $query->where('surface', '>=', $surface);
+        }
+
+        if($rooms = $request->validated('rooms')){
+            $query->where('rooms', '>=', $rooms);
+        }
+
+        if($title = $request->validated('title')){
+            $query->where('title', 'like', '%' . $title . '%');
         }
 
         return view('property.index', 
