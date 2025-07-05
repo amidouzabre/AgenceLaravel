105dd2946e70fd9f987a5cf50bf3fea0930e08e7
amidou.zabre <amidou.zabre@gmail.com>
2025-07-05 09:51:12 +0000

    Vues: create base.blade.php

---
+++ resources/views/base.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>@yield('title') | MonAgence</title>
+</head>
+
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Agence</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    @php 
+        $route = request()->route()->getName();
+    @endphp
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a  href="/" @class(['nav-link', 'active' => str_contains($route, 'property')]) >
+            Biens
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
+</nav>
+
+
+<body>
+    @yield('content')
+</body>
+</html>
