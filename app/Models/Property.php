7198488acc376c1b3d70138af965f64f82027d7d
amidou.zabre <amidou.zabre@gmail.com>
2025-07-07 15:54:43 +0000

    Create Picture Model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use  Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use \Illuminate\Support\Str;
 
 class Property extends Model
@@ -33,5 +34,33 @@ class Property extends Model
     public function getSlug(): string
     {
         return Str::slug($this->title);
+    }
+
+    public function pictures(): HasMany
+    {
+        return $this->hasMany(Picture::class);
+    }
+
+    public function attachFiles(array $files)
+    {
+        $pictures = [];
+        foreach($files as $file) {
+            if($file->getError()){
+                continue;
+            }
+            $filename = $file->store('properties/', $this->id, 'public');
+            $pictures[] = [
+                'filename' => $filename
+            ];
+        }
+
+        if(count($pictures) > 0){
+            $this->pictures()->createMany($pictures);
+        }
+    }
+
+    public function getPicture(): ? Picture
+    {
+        return $this->pictures[0] ?? null;
     }
 }
