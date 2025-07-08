84c8da2bd92489f10c78112f1bf52b48bbf48264
amidou.zabre <amidou.zabre@gmail.com>
2025-07-08 10:06:34 +0000

    User model : add password castes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,8 +11,11 @@ class HomeController extends Controller
     public function index()
     {
         #$properties = Property::with('pictures')->where('sold', false)->orderBy('created_at', 'desc')->limit(4)->get();
+        $user = User::first();
         $properties = Property::available(true)->recent()->limit(4)->get();
-        dd($properties->first()->sold);
+        $user = User::first();
+        $user->password = '0000';
+        dd($user->password, $user);
         return view('home', ['properties' => $properties]);
     }
 }
