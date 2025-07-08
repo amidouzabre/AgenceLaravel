4272d1385ef136d244bc28ba9c4891971a0dee9c
amidou.zabre <amidou.zabre@gmail.com>
2025-07-08 15:23:24 +0000

    Create Component Input

--- resources/views/property/show.blade.php
+++ resources/views/property/show.blade.php
@@ -17,7 +17,7 @@
         <form action="{{ route('property.contact', $property) }}" method="post" class="vstack gap-3">
             @csrf
             <div class="row">
-                @include('shared.input', ['class' => 'col', 'name' => 'firstname', 'label' => 'Prénom']) 
+                <x-input class="col" name="firstname" label="Prénom" />
                 @include('shared.input', ['class' => 'col', 'name' => 'lastname', 'label' => 'Nom']) 
             </div>
             <div class="row">
