117441812d5708174c606e16eb73e9894852fe43
amidou.zabre <amidou.zabre@gmail.com>
2025-07-08 11:46:26 +0000

    Create OptionFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Option;
 use App\Models\Property;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
@@ -18,7 +21,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'email' => 'admin@admin.com',
         ]);
+        $options  = Option::factory(10)->create();
+        Property::factory(50)
+            ->hasAttached($options->random(3))
+            ->create();
 
-        Property::factory(50)->create();
+        
     }
 }
